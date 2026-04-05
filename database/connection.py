07e702b4ab52bad05aa35020546726817a11d071
Alex Maia <iamsralex@gmail.com>
2026-04-05 15:53:26 -0300

    Adicionando o grafo

--- database/connection.py
+++ database/connection.py
@@ -1,9 +1,12 @@
 import os
 from dotenv import load_dotenv
+from osmnx import graph
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from deploy import get_resource_and_client
 from database.dynamo_table import table_exists, create_table, TABLE_NAME
+from database.create_graph import load_graph_cache, download_graph, save_graph_cache
+from pathlib import Path
 
 load_dotenv()
 
@@ -40,3 +43,16 @@ def get_session_dynamo():
         yield table
     finally:
         pass
+
+
+# Criando o grafo pelo OSMnx
+graph_cache_path = Path("cache/cache_graph.graphml")
+
+graph = load_graph_cache(graph_cache_path)
+if graph is None:
+    graph = download_graph("São Paulo, Brazil", "drive")
+    save_graph_cache(graph, graph_cache_path)
+
+
+def get_graph():
+    return graph
---
+++ database/create_graph.py
@@ -0,0 +1,29 @@
+import osmnx as ox
+from pathlib import Path
+
+
+
+def load_graph_cache(graph_path: Path):
+    if graph_path.exists():
+        print(f"[cache] Loading graph from '{graph_path}' …")
+        graph = ox.load_graphml(graph_path)
+        print("[cache] Done")
+        return graph
+    return None
+
+
+def save_graph_cache(graph, graph_path: Path):
+    graph_path.parent.mkdir(parents=True, exist_ok=True)
+    print(f"[cache] Saving graph to '{graph_path}' …")
+    ox.save_graphml(graph, graph_path)
+    print("[cache] Done")
+
+
+# Puxando o grafo da OpenStreetMap usando OSMnx
+def download_graph(place: str, network_type: str):
+
+    print(f"[download] Fetching '{network_type}' network for '{place}' …")
+    graph = ox.graph_from_place(place, network_type=network_type)
+    print(f"[download] Done")
+
+    return graph
